98718a1a23128c92b242bf2b583f257a53cb1e2a
jlooper <jen.looper@gmail.com>
2016-11-02 20:10:49 -0400

    refactoring so as to update to latest Angular

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,26 +1,25 @@
-import {Component} from "angular2/core";
+import {Component} from "@angular/core";
 import {Todo, TodoStore} from "./services/todo";
 import {FooterComponent} from "./components/footer/footer.component";
-
-var explosion = require("nativescript-explosionfield");
 
 @Component({
     selector: "my-app",
     templateUrl: "app.html",
     styleUrls: ['app-common.css', 'app.css'],
-    providers: [TodoStore],
-    directives: [FooterComponent]
+    providers: [TodoStore]
 })
 export class AppComponent {
-    public todoStore: TodoStore
+    public todoStore: TodoStore;
     public newTodoText: string = '';
     
     constructor(_todoStore: TodoStore) {
         this.todoStore = _todoStore;
+        console.log(JSON.stringify(this.todoStore))
     }
     
     addTodo() {
         this.todoStore.add(this.newTodoText);
+        console.log(this.newTodoText);
         this.newTodoText = "";       
     }
     
@@ -30,7 +29,6 @@ export class AppComponent {
 
     remove(event, todo: Todo) {
         if (event.object.android) {
-            explosion.explode(event.object._parent);
             setTimeout(() => {
                 this.todoStore.remove(todo);
             }, 500);
--- app/services/todo.ts
+++ app/services/todo.ts
@@ -1,4 +1,4 @@
-import {Injectable} from "angular2/core";
+import {Injectable} from "@angular/core";
 import * as localStorage from 'application-settings';
 
 export class Todo {
